8aae2c1ab20a22acc4bed3a921d871400721e3a8
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2021-12-09 09:19:41 +0100

    Update

--- main.R
+++ main.R
@@ -24,11 +24,21 @@ ggplot(data=df_preExperiment, aes(x=step., y=n.sick.people, group=run.number.)) 
   ylab("Number of infected people") +
   ggtitle("Number of infected people over time") + #give the plot a title
   scale_colour_manual("Run",values = rainbow(max(df_preExperiment$run.number., na.rm = FALSE)))
-ggsave("./diagrams/pre-ex/arrayOfCurves.png")
+# ggsave("./diagrams/pre-ex/arrayOfCurves.png")
 
 ggplot(data=df_preExperiment, aes(x=step., y=n.sick.people, group=round(step./7))) + #use myDataFrame for the data, columns for x and y
   geom_boxplot() +
   xlab("Days") +  #specify x and y labels
   ylab("Number of infected people") +
-  ggtitle("Number of infected people over time")
-ggsave("./diagrams/pre-ex/boxplot.png")
+  ggtitle("Number of infected people over time (7 day bins)")
+# ggsave("./diagrams/pre-ex/boxplot.png")
+
+duration = aggregate(df_preExperiment$step., by = list(df_preExperiment$run.number.), max)
+colnames(duration)[0] = 'run'
+colnames(duration)[1] = 'duration'
+
+duration
+
+ggplot(data = duration, aes(x='run', y=x)) + geom_boxplot()
+
+duration[duration < 600]
