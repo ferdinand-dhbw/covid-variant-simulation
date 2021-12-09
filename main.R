dc038550e4eba07e63b0321c1095006934c6f341
ferdinand-dhbw <65330418+ferdinand-dhbw@users.noreply.github.com>
2021-12-09 13:39:31 +0100

    Add distribution plot

--- main.R
+++ main.R
@@ -24,14 +24,14 @@ ggplot(data=df_preExperiment, aes(x=step., y=n.sick.people, group=run.number.)) 
   ylab("Number of infected people") +
   ggtitle("Number of infected people over time") + #give the plot a title
   scale_colour_manual("Run",values = rainbow(max(df_preExperiment$run.number., na.rm = FALSE)))
-# ggsave("./diagrams/pre-ex/arrayOfCurves.png")
+ggsave("./diagrams/pre-ex/arrayOfCurves.png")
 
-ggplot(data=df_preExperiment, aes(x=step., y=n.sick.people, group=round(step./7))) + #use myDataFrame for the data, columns for x and y
+ggplot(data=df_preExperiment, aes(x=step., y=n.sick.people, group=round(step./7))) +
   geom_boxplot() +
   xlab("Days") +  #specify x and y labels
   ylab("Number of infected people") +
   ggtitle("Number of infected people over time (7 day bins)")
-# ggsave("./diagrams/pre-ex/boxplot.png")
+ggsave("./diagrams/pre-ex/boxplot.png")
 
 duration = aggregate(df_preExperiment$step., by = list(df_preExperiment$run.number.), max)
 colnames(duration)[0] = 'run'
@@ -39,6 +39,13 @@ colnames(duration)[1] = 'duration'
 
 duration
 
-ggplot(data = duration, aes(x='run', y=x)) + geom_boxplot()
+ggplot(data = duration, aes(x='run', y=x)) +
+  geom_boxplot() +
+  ylab("Duration of epidemic in days") +
+  xlab("runs") +
+  ggtitle("Distribution of duration")
+ggsave("./diagrams/pre-ex/duration.png")
 
-duration[duration < 600]
+# 24 times, the virus was wiped out earlier
+# According to the previous boxplot these are still outliers
+lengths(duration[duration$x < 720, ])
